0072b0622d8a9a692b94900552c1872a7bc492e1
Mark Schabacker <mark.schabacker@gmail.com>
2013-07-13 20:20:07 -0400

    Add newlines to integration spec expectations

--- spec/integration/integration_spec.rb
+++ spec/integration/integration_spec.rb
@@ -6,23 +6,23 @@ describe "The Seven Segment Display Formatter (Integration)" do
 
   it "outputs the correct string for 012345 (multiplier = 2)" do
     expected_output = \
-" --        --   --        -- "\
-"|  |    |    |    | |  | |   "\
-"|  |    |    |    | |  | |   "\
-"           --   --   --   -- "\
-"|  |    | |       |    |    |"\
-"|  |    | |       |    |    |"\
-" --        --   --        -- "
+" --        --   --        -- \n"\
+"|  |    |    |    | |  | |   \n"\
+"|  |    |    |    | |  | |   \n"\
+"           --   --   --   -- \n"\
+"|  |    | |       |    |    |\n"\
+"|  |    | |       |    |    |\n"\
+" --        --   --        -- \n"
     seven_segment_display_formatter.format("012345", 2).should == expected_output
   end
 
   it "outputs the correct string for 6789 (multiplier = 1)" do
     expected_output = \
-" -   -   -   - "\
-"|     | | | | |"\
-" -       -   - "\
-"| |   | | |   |"\
-" -       -   - "
+" -   -   -   - \n"\
+"|     | | | | |\n"\
+" -       -   - \n"\
+"| |   | | |   |\n"\
+" -       -   - \n"
     seven_segment_display_formatter.format("6789", 1).should == expected_output
   end
 
