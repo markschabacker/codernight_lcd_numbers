7ae73dad00c207680b0a31ebf3d182b106769c58
Mark Schabacker <mark.schabacker@gmail.com>
2013-07-14 08:36:08 -0400

    Initialize SevenSegmentCharacters members on access

--- lib/seven_segment_characters.rb
+++ lib/seven_segment_characters.rb
@@ -1,108 +1,92 @@
 require_relative 'seven_segment_character.rb'
 
 class SevenSegmentCharacters
-  @@charNil = SevenSegmentCharacter.new
-  @@char0 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "x x"\
-                                                         "x x"\
-                                                         "x x"\
-                                                         "xxx")
-
-  @@char1 = SevenSegmentCharacter.initialize_from_string("   "\
-                                                         "  x"\
-                                                         "  x"\
-                                                         "  x"\
-                                                         "   ")
-
-  @@char2 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "  x"\
-                                                         "xxx"\
-                                                         "x  "\
-                                                         "xxx")
-
-  @@char3 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "  x"\
-                                                         "xxx"\
-                                                         "  x"\
-                                                         "xxx")
-
-  @@char4 = SevenSegmentCharacter.initialize_from_string("   "\
-                                                         "x x"\
-                                                         "xxx"\
-                                                         "  x"\
-                                                         "   ")
-
-  @@char5 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "x  "\
-                                                         "xxx"\
-                                                         "  x"\
-                                                         "xxx")
-
-  @@char6 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "x  "\
-                                                         "xxx"\
-                                                         "x x"\
-                                                         "xxx")
-
-  @@char7 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "  x"\
-                                                         "  x"\
-                                                         "  x"\
-                                                         "   ")
-
-  @@char8 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "x x"\
-                                                         "xxx"\
-                                                         "x x"\
-                                                         "xxx")
-
-  @@char9 = SevenSegmentCharacter.initialize_from_string("xxx"\
-                                                         "x x"\
-                                                         "xxx"\
-                                                         "  x"\
-                                                         "xxx")
 
   def self.charNil
-    @@charNil
+    @@charNil ||= SevenSegmentCharacter.initialize_from_string("   "\
+                                                               "   "\
+                                                               "   "\
+                                                               "   "\
+                                                               "   ")
   end
 
   def self.char0
-    @@char0
+    @@char0 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "x x"\
+                                                             "x x"\
+                                                             "x x"\
+                                                             "xxx")
   end
 
   def self.char1
-    @@char1
+    @@char1 ||= SevenSegmentCharacter.initialize_from_string("   "\
+                                                             "  x"\
+                                                             "  x"\
+                                                             "  x"\
+                                                             "   ")
   end
 
   def self.char2
-    @@char2
+    @@char2 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "  x"\
+                                                             "xxx"\
+                                                             "x  "\
+                                                             "xxx")
   end
 
   def self.char3
-    @@char3
+    @@char3 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "  x"\
+                                                             "xxx"\
+                                                             "  x"\
+                                                             "xxx")
   end
 
   def self.char4
-    @@char4
+    @@char4 ||= SevenSegmentCharacter.initialize_from_string("   "\
+                                                             "x x"\
+                                                             "xxx"\
+                                                             "  x"\
+                                                             "   ")
   end
 
   def self.char5
-    @@char5
+    @@char5 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "x  "\
+                                                             "xxx"\
+                                                             "  x"\
+                                                             "xxx")
   end
 
   def self.char6
-    @@char6
+    @@char6 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "x  "\
+                                                             "xxx"\
+                                                             "x x"\
+                                                             "xxx")
   end
 
   def self.char7
-    @@char7
+    @@char7 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "  x"\
+                                                             "  x"\
+                                                             "  x"\
+                                                             "   ")
   end
 
   def self.char8
-    @@char8
+    @@char8 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "x x"\
+                                                             "xxx"\
+                                                             "x x"\
+                                                             "xxx")
   end
 
   def self.char9
-    @@char9
+    @@char9 ||= SevenSegmentCharacter.initialize_from_string("xxx"\
+                                                             "x x"\
+                                                             "xxx"\
+                                                             "  x"\
+                                                             "xxx")
   end
 end
